0d2fd03ac6d6849f178cb7df45bb53ae163e16f5
yesak1 <23kkazliner@athenian.org>
2020-09-12 04:30:15 +0000

    finished

--- my_code.py
+++ my_code.py
@@ -1,3 +1,10 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
-
+#  none
+personname= input("enter a person's name")
+verb=input('enter a verb')
+adverb=input('enter an adverb')
+food=input('enter a food')
+personname2=input("enter a person's name")
+verb2=input('enter a verb')
+adverb2=input('enter an adverb')
+print (f"There once was a person named {personname} that really liked to {verb} {adverb}. One time, {personname} was eating a {food} and realized that {personname2} wanted to {verb2} \n with them. So {personname} and {personname2} {verb2}ed together {adverb2}")
